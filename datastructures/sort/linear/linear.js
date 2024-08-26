d44db453c672567eb2fbdfedb0416b5eafba43cc
= <=>
2024-08-26 07:24:37 -0400

    Data Structures

---
+++ datastructures/sort/linear/linear.js
@@ -0,0 +1,6 @@
+//Array
+const linear = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+for (var i=0; i < linear.length; i++){
+    console.log(linear[i]);
+}
---
+++ datastructures/sort/linear/linearsort.js
@@ -0,0 +1,16 @@
+//var array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+function linearsort(array, key){
+    var array = [];
+    var key = "";
+
+    for (var i=0; i < array.length; i++){
+        console.log(array[i]);
+        if (key == array[i])
+            return console.log("FOUND IT!", array[i])
+
+ 
+    }
+}
+
+export {linearsort};
